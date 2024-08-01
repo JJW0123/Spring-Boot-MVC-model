96877881be96bbf356b7f90136d68feea7a85256
전지웅 <wjswldnd2000@naver.com>
2024-08-01 16:36:04 +0900

    MVC 모델을 기반으로 회원 도메인과 리포지토리 작성

---
+++ src/main/java/spring_example/hello_spring/domain/Member.java
@@ -0,0 +1,11 @@
+package spring_example.hello_spring.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    private Long id;
+    private String name;
+}
